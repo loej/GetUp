87bbed11fd597c099411d1086f144dd8fc6901f1
loej <jfm203@scarletmail.rutgers.edu>
2020-07-18 20:26:27 -0400

    Added redirect.

--- src/hello.py
+++ src/hello.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+# redirect and url_for allow the user to immediatley get redirected from a different url
+
 # Imports Flask instance 
 app = Flask(__name__)
 
@@ -13,6 +15,19 @@ def mainPage():
 @app.route('/<name>')
 def userInterface(name):
     return f'Hello {name}'
+
+# GetUp route where the user selects the time. 
+@app.route('/getup')
+def getUpPage():
+    return f'<h1> Welcome to Get Up! </h1>'
+    
+# Redirecting user to new page:
+# Is a user tries to access the /admin page it will redirect them to 
+# /getup
+def admin():
+    # A redirect simply redirects the user to a new screen. 
+    return redirect(url_for('getUpPage'))
+
 
 # Main function.
 if __name__ == "__main__":
