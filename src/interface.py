09dc6fbd13fc4e0266f9987dd896b118386f331b
loej <joel.martinez@rutgers.edu>
2020-07-19 18:43:45 -0400

    Added eye icon.

--- src/interface.py
+++ src/interface.py
@@ -2,35 +2,44 @@ from flask import Flask, redirect, url_for, render_template
 # redirect and url_for allow the user to immediatley get redirected from a different url
 # render_template allows to render an html file
 
-# Imports Flask instance 
+# Imports Flask instance
 app = Flask(__name__)
 
-# Defines how to access scpecific page 
+# Defines how to access scpecific page
 # This is the route: the default domain.
+
+
 @app.route('/')
 def mainPage():
     return redirect(url_for('getUpPage'))
 
-# GetUp route where the user selects the time. 
+# GetUp route where the user selects the time.
+
+
 @app.route('/getup')
 def getUpPage():
     # Renders file inside template/index.html
     return render_template('index.html')
 
 # This is a new page that accepts any input inside the brackets
-# Out puts what the function recieves from the parameter in the URL. 
+# Out puts what the function recieves from the parameter in the URL.
+
+
 @app.route('/<name>')
 def userInterface(name):
-    return f'You better be: {name}'
+    return f'This page is for {name}'
 
 # Redirecting user to new page:
-# Is a user tries to access the /admin page it will redirect them to 
+# Is a user tries to access the /admin page it will redirect them to
 # /getup
+
+
 def admin():
-    # A redirect simply redirects the user to a new screen. 
+    # A redirect simply redirects the user to a new screen.
     # name=' ' allows to pass the admin NAME to print the function userInterface()
-    return redirect(url_for('mainPage', name= 'ADMIN.'))
+    return redirect(url_for('mainPage', name='ADMIN.'))
+
 
 # Main function.
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
